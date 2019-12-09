7d699526232850fe7cf9129617bffd066ca23668
Mark <mwhelan1988@gmail.com>
2019-12-09 09:07:07 -0800

    v0.0.1
    -Added index.php file
    -Updated README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SSP1302-MajorExample
 SSP1302 in class example
+
+## This is a subheading 
+where you can describe the project more. 
---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+    
+    </head>
+
+    <body>
+        <h1>
+            <?php echo "<h1>Hello World</h1>"; ?>
+        </h1>
+    </body>
+
+
+
+</html>
